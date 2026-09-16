f3b319ff2e36fcfd00c04c643163ce28c97b37c8
Enemeka <meenwachukwu@gmail.com>
2026-09-16 13:48:30 +0100

    a deployment using environmental variables

---
+++ env-variables/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express()
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send(`Hello from express ${process.env.APP_NAME}`);
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`)
+})
